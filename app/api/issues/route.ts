5b0ba017bfaafaa853027d858e850410cbd8a66d
somukhan <mhsomrat154@gmail.com>
2023-12-04 01:40:11 +0600

    fetch data at server side

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -21,3 +21,8 @@ export async function POST(request: NextRequest) {
 
   return NextResponse.json(newIssue, { status: 201 })
 }
+
+export async function GET(request: NextRequest) {
+  const issues = await prisma.issue.findMany()
+  return NextResponse.json({ issues }, { status: 200 })
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,22 @@
 import { Button } from '@radix-ui/themes'
 import Link from 'next/link'
 
-export default function IssuesPage() {
+export default async function IssuesPage() {
+  const response = await fetch('http://localhost:3000/api/issues', {
+    method: 'GET',
+  })
+  const { issues } = await response.json()
+
+  console.log(issues)
+
   return (
-    <div>
+    <section>
       <Button>
         <Link href="/issues/create">Create Issue</Link>
       </Button>
-    </div>
+      {issues.map((issue: any) => (
+        <p key={issue.id}>{issue.title}</p>
+      ))}
+    </section>
   )
 }
