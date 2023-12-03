58f2ec1ef7d729d3d5e776d0f7565e751fc01b71
somukhan <mhsomrat154@gmail.com>
2023-12-03 16:37:25 +0600

    setup navbar in the layout.tsx file

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,3 +1,18 @@
+import Link from 'next/link'
+
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav className="flex">
+      <Link href="/">Logo</Link>
+      <ul className="flex">
+        <li>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/issues">Issues</Link>
+        </li>
+        <li></li>
+      </ul>
+    </nav>
+  )
 }
